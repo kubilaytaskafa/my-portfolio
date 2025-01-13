4d7ef7e8eccd73f78e0f719b375866f17c896a67
kubilay taşkafa <info.kubilay0@gmail.com>
2025-01-13 18:05:14 +0300

    portfolio-1.0.3

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,23 +1,28 @@
-import { Link } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
 const Buttons = () => {
+  const navigate = useNavigate();
   return (
     <div className="flex items-center justify-start flex-col text-white gap-5 bg-[#171717] min-h-screen text-center">
-      <Link to={"/Hakkımda"}>
-        <button className="mt-4 ml-4 px-3 py-2 sm:px-4 sm:py-2 md:px-5 md:py-2.5 rounded-md bg-[#171717] hover:bg-[#323232] transition duration-300 text-sm sm:text-base md:text-lg">
-          Hakkımda
-        </button>
-      </Link>
-      <Link to={"/Projelerim"}>
-        <button className="mt-2 ml-4 px-3 py-2 sm:px-4 sm:py-2 md:px-5 md:py-2.5 rounded-md bg-[#171717] hover:bg-[#323232] transition duration-300 text-sm sm:text-base md:text-lg">
-          Projelerim
-        </button>
-      </Link>
-      <Link to={"/Eğitim"}>
-        <button className="mt-2 ml-4 px-3 py-2 sm:px-4 sm:py-2 md:px-5 md:py-2.5 rounded-md bg-[#171717] hover:bg-[#323232] transition duration-300 text-sm sm:text-base md:text-lg">
-          Eğitim
-        </button>
-      </Link>
+      <button
+        className="mt-4 ml-4 px-3 py-2 sm:px-4 sm:py-2 md:px-5 md:py-2.5 rounded-md bg-[#171717] hover:bg-[#323232] transition duration-300 text-sm sm:text-base md:text-lg"
+        onClick={() => navigate("/Hakkımda")}
+      >
+        Hakkımda
+      </button>
+
+      <button
+        className="mt-2 ml-4 px-3 py-2 sm:px-4 sm:py-2 md:px-5 md:py-2.5 rounded-md bg-[#171717] hover:bg-[#323232] transition duration-300 text-sm sm:text-base md:text-lg"
+        onClick={() => navigate("/Projelerim")}
+      >
+        Projelerim
+      </button>
+
+      <button
+        className="mt-2 ml-4 px-3 py-2 sm:px-4 sm:py-2 md:px-5 md:py-2.5 rounded-md bg-[#171717] hover:bg-[#323232] transition duration-300 text-sm sm:text-base md:text-lg"
+        onClick={() => navigate("/Eğitim")}
+      >
+        Eğitim
+      </button>
     </div>
   );
 };
